8f6e8f3ad002efc942803967dcd0dce4a2fa200f
ProtoByter <20995169+ProtoByter@users.noreply.github.com>
2021-06-21 17:05:03 +0100

    Move away from portmidi (didn't work on windows

--- build.rs
+++ build.rs
@@ -3,8 +3,8 @@ use cmake::Config;
 
 fn main()
 {
-    let dst = Config::new("portmidi").define("BUILD_TESTING","NO").build_target("install").build();
+    //let dst = Config::new("portmedia/portmidi/trunk").define("BUILD_TESTING","NO").build_target("portmidi-dynamic").build();
 
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=dylib=portmidi");
+    //println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    //println!("cargo:rustc-link-lib=dylib=portmidi");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 extern crate portmidi as pm;
+use pm::MidiMessage;
 
 fn main() {
     let ctx = match pm::PortMidi::new() {
         Ok(ctx) =>  ctx,
         Err(_) => panic!("Couldn't create portmidi context")
     };
-    println!("Hello, world!");
+
+    for dev in ctx.devices().unwrap() {
+        println!("{}", dev);
+    }
+
+    let mut device_str = String::new();
+
+    std::io::stdin()
+        .read_line(&mut device_str)
+        .expect("Failed to read");
+
+    println!("{}",device_str);
 }
